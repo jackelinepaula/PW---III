a04140d4d9f9cf20cc46abb479ac0b4634282b36
Jackeline Paula <kpopcherry123@gmail.com>
2021-12-07 23:29:38 -0300

    routes laravel

---
+++ routes/resources/views/contato.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
+
+    <title>Contato</title>
+</head>
+<body>
+    <header class="header header-ctt">
+        <h3>Agora, você está em Contato!</h3>
+        <a href="/"><button>Voltar para a home</button></a>
+    </header>
+</body>
+</html>
